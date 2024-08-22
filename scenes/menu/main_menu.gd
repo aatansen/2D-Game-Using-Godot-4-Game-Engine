76c23a51fabf76298c2e426063640f5e12b282dd
Md. Alahi Almin Tansen <aatansen@gmail.com>
2024-08-22 22:28:46 +0600

    Organized files and new level 3 gameplay working now

---
+++ scenes/menu/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func _on_btn_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
+
+
+func _on_btn_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
+
+
+func _on_btn_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level3.tscn")
